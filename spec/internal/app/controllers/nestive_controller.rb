c913b3f66e476beaaafec40a1954f434d70a4b8c
Justin Morris <justin@pixelbloom.com>
2013-06-16 20:12:28 +1000

    Add specs covering rails 4 breakage

--- spec/internal/app/controllers/nestive_controller.rb
+++ spec/internal/app/controllers/nestive_controller.rb
@@ -2,8 +2,12 @@ class NestiveController < ApplicationController
   def extended_one
     render :layout => 'extend_one'
   end
-  
+
   def extended_two
     render :layout => 'extend_two'
+  end
+
+  def extend_from_view
+    render :template => "nestive/extend_from_view", :layout => false
   end
 end
