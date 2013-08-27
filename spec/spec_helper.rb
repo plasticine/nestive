47d27e2176753068153841dc5550f44ad71ad476
plasticine <justin@pixelbloom.com>
2013-08-27 12:24:30 +1000

    Fix quality checking

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,8 @@
 ENV['RAILS_ENV'] ||= 'test'
 
 if ENV['COVERAGE']
-  require_relative (ENV['TRAVIS'] ? 'use_coveralls' : 'use_simplecov')
+  require_relative 'use_coveralls' if ENV['TRAVIS']
+  require_relative 'use_simplecov'
 end
 
 require 'bundler/setup'
